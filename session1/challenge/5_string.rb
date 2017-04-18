cbb2b216790e5fcb3bc0673d079a4724bc6efbc3
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-18 13:33:06 +0100

    Updates solution to 1:5 -however duplicative solution, look to improve in future

--- session1/challenge/5_string.rb
+++ session1/challenge/5_string.rb
@@ -1,8 +1,19 @@
 # Given a string, replace every instance of sad to happy
-# 
+#
 # add_more_ruby("The clowns were sad.")         # => "The clowns were happy."
 # add_more_ruby("The sad dad said sad stuff.")  # => "The happy dad said happy stuff."
 # add_more_ruby("Sad times are ahead!")         # => "Happy times are ahead!"
 
 def add_more_ruby(string)
+  match = "sad"
+  while(string.include?(match))
+    string.sub!(match, "happy")
+  end
+  string
+
+  match = "Sad"
+  while(string.include?(match))
+    string.sub!(match, "Happy")
+  end
+  string
 end
