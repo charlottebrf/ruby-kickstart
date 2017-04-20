2661394f2d06f85fe8c2c5cc4efbc17f9a59f0bd
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-20 20:16:40 +0100

    Updates solution to 2:3

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -6,6 +6,32 @@
 # "".every_other_char         # => ""
 
 class String
+
+  self
+
   def every_other_char
+    string = self.split("")
+    evens = []
+    string.each_with_index do |character,index|
+      if index.even?
+        evens.push(character)
+      end
+    end
+    evens.join
   end
+
 end
+
+String.new.every_other_char
+
+
+
+#pseudocode:
+# split the char into an array
+#enumerate over the index of each char
+#append the even chars into a new array
+#join this array together to become a new string
+#evens = numbers.select { |number| number.even? }
+#
+# evens = string.select { |index| index.even?}
+# evens
