d516b832cc5e02aee5d1b175451507e571278c66
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-20 22:10:45 +0100

    Updates solution for 2:4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,17 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(nums)
+  nums.select { |n| nums.include? n*n }.sort
+end
+
+
+#I want the square number
+#if there is a number in the array that is equal to n * n then I want to append the original square number
+#pseudocode:
+#I have an array
+#I check each element against the original array to see if the array contains any of its squares
+#
+#if it does then i want to push those characters into a new array
+# I the want to return that array
