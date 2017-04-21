bbded8b9301360f26c3b540588be9d7b16acf152
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-21 12:24:55 +0100

    Updates solution to 2:12

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -19,10 +19,83 @@
 # f.denominator = 100
 # f.to_s               # => "50/100"
 # f.to_f               # => 0.5
-
 class Fraction
+
+  attr_accessor 'numerator', 'denominator'
+
+  def initialize(numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
+
+  def to_f
+    (@numerator.to_f/ @denominator.to_f).round(1)
+  end
+
+  def lowest
+    divisor= gcd(@numerator, @denominator)
+    lowest_numerator = @numerator / divisor
+    lowest_denominator = @denominator / divisor
+    lowest_fraction = Rational(lowest_numerator, lowest_denominator)
+    lowest_fraction
+  end
+
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+
+  def to_s
+    @numerator.to_s + "/" + @denominator.to_s
+  end
+
 end
+
+#My solution with notes:
+# class Fraction
+#
+#   attr_accessor 'numerator', 'denominator'
+#
+#   def initialize(numerator, denominator)
+#     @numerator = numerator
+#     @denominator = denominator
+#   end
+#
+#
+#   def to_f
+#     (@numerator.to_f/ @denominator.to_f).round(1)
+#       # Also include a method called to_f that returns the ratio as a float (a number with a decimal point, such as 10.3)
+#       #double check that the round isn't rounding it up/ down in a way that tests don't pass
+#   end
+#
+#   def lowest
+#     divisor= gcd(@numerator, @denominator)
+#     lowest_numerator = @numerator / divisor
+#     lowest_denominator = @denominator / divisor
+#     lowest_fraction = Rational(lowest_numerator, lowest_denominator)
+#     lowest_fraction
+#
+#       #Part 3:
+#   # Also include a method called lowest, which returns a new Fraction, where the numerator and denominator
+#   # are reduced to lowest terms (ie 20/60 becomes 1/3)
+#   # This will require finding the greatest common divisor for the numerator and denominator
+#   # then dividing both by that number.
+#   # I have included a greatest common divisor method for you
+#   end
+#
+#   def gcd(a,b)
+#     return a if b == 0
+#     gcd(b, a%b)
+#   end
+#
+#   def to_s
+#     @numerator.to_s + "/" + @denominator.to_s
+#   end
+#   # fraction = Rational(@numerator, @denominator)
+#   # fraction.to_s
+#    #Part 4:
+#   # You should also define a method, to_s, that will represent the Fraction as a String
+#
+#
+# end
