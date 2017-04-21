3ed40d698fe0b0b695dfe3633f54c34967a8a798
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-21 11:26:12 +0100

    Updates solution to 2:10

--- session2/challenge/10_classes.rb
+++ session2/challenge/10_classes.rb
@@ -15,5 +15,51 @@
 # josh.age      # => 30
 #
 
+
+
 class Person
+  attr_accessor 'name', 'age'
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def birthday
+    @age += 1
+  end
+
 end
+
+
+# class Person
+#   attr_accessor 'name', 'age', 'birthday'
+#
+#   def initialize(name, age, birthday)
+#     name = @name
+#     age = @age
+#     birthday = @birthday
+#   end
+#
+# end
+
+
+# Person.new.name.age.birthday
+
+# # new_person = Person.new("charlotte", 31, 16)
+# new_person.name
+# new_person.age
+# new_person.birthday
+
+#
+# def name=(name)
+#   @name = name
+# end
+#
+# def age=(age)
+#   @age = age
+# end
+#
+# def birthday=(birthday)
+#   @birthday = birthday
+# end
