d910a8beca9f626de006fbbdc0c6dc136d3e918f
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-23 16:43:55 +0100

    Updates solution to 3:3, along with my commented out failed attempt

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,3 +8,35 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
+def staircase(num)
+  range_array = []
+  range = (1..num).to_a #these are all of my keys
+  p range
+  odds_key = range.select { |num| num.odd? }
+  p odds_key
+
+  evens_value = []
+  odds_key.each do |odd_num|
+    odds_range = (1..odd_num).to_a
+    evens_value << odds_range.select { |num| num.even? }
+  end
+   p evens_value
+
+  #  << that shovels whatever is on the right as a single thing. See if there is a method on array that can add all the elements from one array to another
+
+  # http://stackoverflow.com/questions/1801516/how-do-you-add-an-array-to-another-array-in-ruby-and-not-end-up-with-a-multi-dim
+
+  p odds_evens_key = {odds_key => evens_value}
+end
+
+staircase(3)
+
+#given an integer
+#take this integer & create a new array of all the non
+#then need to get the range of all the non-negative integers that make up that number
+#create a new array with all of these integers
+#for each of these integers loop over them in turn to give the range of all the non-negative integers that make up that number
+#shovel these into a new array
+#take this new array & select only the even numbers, greater than 1
+#take this array & create a hash key for the original integer => [array of evens]
+#merge these individual hash keys into a single hash key & return
