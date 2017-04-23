aeddb70d8eb2122269a16cde5f2f3793c1b7b254
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-23 21:38:55 +0100

    Updates solution to 3:7, along with my incorrect original solution in comments

--- session3/challenge/7_hashes.rb
+++ session3/challenge/7_hashes.rb
@@ -24,35 +24,107 @@
 #                      <li style='color:#0000FF;font-family:"Courier New", "Lucida Console"'>football</li>
 #                      </ol>
 
+
+
 class HTMLTag
   FONTS = {
     :serif      => '"Times New Roman", "Georgia"',
     :sans_serif => '"Arial", "Verdana"',
-    :monospace  => '"Courier New", "Lucida Console"'
+    :monospace  => '"Courier New", "Lucida Console"' ,
   }
 
-  attr_accessor :name, :innerHTML, :options
+  COLORS = {
+    :red   => '#FF0000',
+    :green => '#00FF00',
+    :blue  => '#0000FF',
+  }
+
+  attr_accessor :name, :innerHTML, :font, :color, :multiline
 
   # options: :multiline should be true or false
-  def initialize(name, innerHTML, options)
-    @name, @innerHTML, @options = name, innerHTML, options
-  end
-
-  def font
-    font = options[:font]  #  one of :serif, :sans_serif, or :monospace
-    FONTS[font]
+  def initialize(name, innerHTML, options=Hash.new)
+    @name, @innerHTML = name, innerHTML
+    self.font      = FONTS[options[:font]]
+    self.color     = COLORS[options[:color]]
+    self.multiline = options.fetch :multiline, false
   end
 
   def style
-    return nil unless options[:font]
-    "style='font-family:#{font}'"
+    return nil unless font || color
+    to_return = "style='"
+    to_return << "font-family:#{font};" if font
+    to_return << "color:#{color};"      if color
+    to_return << "'"
+    to_return
   end
 
   def to_s
-    line_end = if options[:multiline] then "\n" else "" end
-    "<#{name} #{style}>#{line_end}"  \
-    "#{innerHTML.chomp}#{line_end}"  \
+    line_end = ""
+    line_end = "\n" if multiline # remember, if options[:multiline] doesn't exist, it will return nil, and nil is false
+    "<#{name} #{style}>#{line_end}"   \
+      "#{innerHTML.chomp}#{line_end}" \
     "</#{name}>\n"
   end
-
 end
+
+# This was my original incorrect solution. Again I keep not getting .self in classes
+# Also inserting the Hash as an optional argument & then including the font & colors as options
+#along with their symbols
+#stle I got that it needed to be or but didn't properly carry out the login in the return strings
+#same goes for to_s method- covering all of the eventualities
+#Need to improve looking at the instance variable examples & truly understanding how that would translate to a class that needs to be amended
+
+
+# class HTMLTag
+#
+#   # modify it such that it also accepts in the hash, a color (whose values are red("#FF0000"), green("#00FF00"), and blue(#0000FF) )
+#   FONTS = {
+#     :serif      => '"Times New Roman", "Georgia"',
+#     :sans_serif => '"Arial", "Verdana"',
+#     :monospace  => '"Courier New", "Lucida Console"'
+#   }
+#
+# #why is FONTS in a capital?
+# #difference between storing hashes in a symbol- check if this needs to be as it appears above?
+#   COLORS = {
+#     :red     => "#FF0000",
+#     :green   => "#00FF00",
+#     :blue    => "#0000FF"
+#   }
+#
+#   # if the color is set, then it should show up in the style
+#   # It should also not be necessary to pass in the hash, if you don't want to specify options
+#   #
+#
+#
+#   attr_accessor :name, :innerHTML, :options
+#
+#   # options: :multiline should be true or false
+#   def initialize(name, innerHTML, options)
+#     @name, @innerHTML, @options = name, innerHTML, options
+#   end
+#
+#   def font
+#     font = options[:font]  #  one of :serif, :sans_serif, or :monospace
+#     FONTS[font]
+#   end
+#
+#   def color
+#     color = options[:color] #one of :red, :green, or :blue
+#     COLORS[color]
+#   end
+#
+#   def style
+#     return nil unless options[:font] || options[:color]
+#     "style='font-family:#{font}'"
+#     "style='color:#{color}'"
+#   end
+#
+#   def to_s
+#     line_end = if options[:multiline] then "\n" else "" end
+#     "<#{name} #{style}>#{line_end}"  \
+#     "#{innerHTML.chomp}#{line_end}"  \
+#     "</#{name}>\n"
+#   end
+#
+# end
