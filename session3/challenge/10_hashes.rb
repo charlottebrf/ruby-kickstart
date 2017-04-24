f0c3f3d80ffb49beff7637331ea97644a47b8253
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-24 07:08:18 +0100

    Updates solution to 3:10, alongside comments which aim to unpick each step

--- session3/challenge/10_hashes.rb
+++ session3/challenge/10_hashes.rb
@@ -24,10 +24,35 @@
 # pathify 'usr' => {'bin' => ['ruby'], 'include' => ['zlib.h'] }                               # => ['/usr/bin/ruby', '/usr/include/zlib.h']
 # pathify 'usr' => {'bin' => ['ruby']}, 'opt' => {'local' => {'bin' => ['sqlite3', 'rsync']} } # => ['/usr/bin/ruby', 'opt/local/bin/sqlite3', 'opt/local/bin/rsync']
 # pathify                                                                                      # => []
-#
+
 #
 # create it from scratch :)
 
 
-def pathify
+def pathify(paths=Hash.new)
+
+  #base step
+  return paths.map { |path| '/' + path } if paths.is_a? Array
+  #return the new array of the hash table- for each path add before it a '/' (this is a block)
+  #having crreated that then check if the path is an array
+  #if the path is an array then return as an array
+
+  #recursive step
+
+  #now that the base step of creating the array has been done- then carry out this recursive step
+  #for each of the paths keys- which is now an array due to the base step
+  #iterate over each of the parent_path & child_dirs
+  #these have been specified inside the loop- below .each
+  to_return = []
+  paths.each do |parent_path, child_dirs|
+    parent_path = '/' + parent_path
+    child_paths = pathify child_dirs
+    #is this invoking the method on child_dirs?
+    child_paths.each do |child_paths|
+      to_return << (parent_path + child_path)
+      #then for each of the child_paths - add the parent_path to the child_path & shovel this into a new array to_return
+    end
+  end
+  to_return
+  #return this final concatanated array containing both parent & child directories
 end
