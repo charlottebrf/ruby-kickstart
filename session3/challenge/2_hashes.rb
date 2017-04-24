d66c01f4db9047fc00d053419eb02a129051e1fe
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-24 10:31:09 +0100

    Updates solution 3:2, along with my commented out so close to passing code

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -8,28 +8,66 @@
 # staircase 4  # => {1 => [], 3 => [2]}
 # staircase 5  # => {1 => [], 3 => [2], 5 =>[2, 4]}
 
-def staircase(num)
-  range_array = []
-  range = (1..num).to_a #these are all of my keys
-  p range
-  odds_key = range.select { |num| num.odd? }
-  p odds_key
-
-  evens_value = []
-  odds_key.each do |odd_num|
-    odds_range = (1..odd_num).to_a
-    evens_value << odds_range.select { |num| num.even? }
+def staircase(n)
+  to_return = {}
+  1.upto n do |crnt_size|
+    next if crnt_size.even?
+    all   = Array.new(crnt_size) { |i| i + 1 }
+    evens = all.select { |i| i.even? }
+    to_return[crnt_size] = evens
   end
-   p evens_value
-
-  #  << that shovels whatever is on the right as a single thing. See if there is a method on array that can add all the elements from one array to another
-
-  # http://stackoverflow.com/questions/1801516/how-do-you-add-an-array-to-another-array-in-ruby-and-not-end-up-with-a-multi-dim
-
-  p odds_evens_key = {odds_key => evens_value}
+  to_return
 end
 
-staircase(3)
+#Very nearly did it! Close but produced a multi-dimensional array rather than a 1d one- overuse of array here where I was shovelling into an array when it should have been a hash.
+# def staircase(num)
+#   range_array = []
+#   range = (1..num).to_a #these are all of my keys
+#
+#   odds_key = range.select { |num| num.odd? }
+#
+#
+#   evens_value = []
+#   odds_key.each do |odd_num|
+#     odds_range = (1..odd_num).to_a
+#     evens_value << odds_range.select { |num| num.even? }
+#   end
+#   evens_value
+# end
+
+# def staircase(num)
+#   range_array = []
+#   range = (1..num).to_a #these are all of my keys
+#   odds_key = range.select { |num| num.odd? }
+#
+#   odds_key.each {|char| Hash[char] = (1..e).to_a.select { |num| num.even? } }
+# end
+#
+# odds_evens_key = {odds_key => evens_value}
+# end
+
+# def staircase(num)
+#   range_array = []
+#   range = (1..num).to_a #these are all of my keys
+#   p range
+#   odds_key = range.select { |num| num.odd? }
+#   p odds_key
+#
+#   evens_value = []
+#   odds_key.each do |odd_num|
+#     odds_range = (1..odd_num).to_a
+#     evens_value << odds_range.select { |num| num.even? }
+#   end
+#    p evens_value
+#
+#   #  << that shovels whatever is on the right as a single thing. See if there is a method on array that can add all the elements from one array to another
+#
+#   # http://stackoverflow.com/questions/1801516/how-do-you-add-an-array-to-another-array-in-ruby-and-not-end-up-with-a-multi-dim
+#
+#   p odds_evens_key = {odds_key => evens_value}
+# end
+#
+# staircase(3)
 
 #given an integer
 #take this integer & create a new array of all the non
